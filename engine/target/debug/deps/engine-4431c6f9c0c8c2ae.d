7fd254c542c59baefd68f17828849e73506bd193
Lukas Bauza <lukasbauza.lb@proton.me>
2026-03-06 13:44:48 +0000

    Chess engine initial commit

---
+++ engine/target/debug/deps/engine-4431c6f9c0c8c2ae.d
@@ -0,0 +1,5 @@
+/home/lukasb/.programming/onlychess/engine/target/debug/deps/engine-4431c6f9c0c8c2ae.d: src/main.rs
+
+/home/lukasb/.programming/onlychess/engine/target/debug/deps/libengine-4431c6f9c0c8c2ae.rmeta: src/main.rs
+
+src/main.rs:
---
+++ engine/target/debug/deps/engine-cf3515c6c06d8ca5.d
@@ -0,0 +1,5 @@
+/home/lukasb/.programming/onlychess/engine/target/debug/deps/engine-cf3515c6c06d8ca5.d: src/main.rs
+
+/home/lukasb/.programming/onlychess/engine/target/debug/deps/libengine-cf3515c6c06d8ca5.rmeta: src/main.rs
+
+src/main.rs:
