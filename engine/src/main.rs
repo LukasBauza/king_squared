22ae8ace78e27fd0d084067b6d53208d8671836f
Lukas Bauza <lukasbauza.lb@proton.me>
2026-03-10 14:59:03 +0000

    Testing bitboard creation

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,3 +1,11 @@
+mod board;
+mod utils;
+
+use crate::board::{Board, PieceType};
+
 fn main() {
-    println!("Hello, world!");
+    let board = Board::new();
+
+    let white_pawns_bb = board.get_piece_bitboard(PieceType::WhitePawns);
+    println!("White pawns bitboard: {:#064b}", white_pawns_bb);
 }
