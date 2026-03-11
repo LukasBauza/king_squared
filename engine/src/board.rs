2a4cb626bf9e5707e85a0f11a6e477c1b0509725
Lukas Bauza <lukasbauza.lb@proton.me>
2026-03-11 11:16:51 +0000

    Display board with piece types

--- engine/src/board.rs
+++ engine/src/board.rs
@@ -1,5 +1,8 @@
+use num_enum::TryFromPrimitive;
+
 #[rustfmt::skip]
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, TryFromPrimitive)]
+#[repr(usize)]
 pub(crate)enum BoardType {
     WhitePawns = 0, WhiteKnights = 1, WhiteBishops = 2, WhiteRooks = 3, WhiteQueens = 4, WhiteKing = 5,
     BlackPawns = 6, BlackKnights = 7, BlackBishops = 8, BlackRooks = 9, BlackQueens = 10, BlackKing = 11,
@@ -7,8 +10,8 @@ pub(crate)enum BoardType {
     // Adding these will save time in calculation, as you don't need to count each array individually.
     WhitePieces = 12,
     BlackPieces = 13,
-    Empty = 14,
-    Occupied = 15
+    Occupied = 14,
+    Empty = 15
 }
 
 pub(crate) struct Board {
@@ -53,6 +56,7 @@ impl Board {
 
         bitboards[BoardType::Occupied as usize] =
             bitboards[BoardType::WhitePieces as usize] | bitboards[BoardType::BlackPieces as usize];
+        // FIX: For some reason this is not being updated properly.
         bitboards[BoardType::Empty as usize] = !bitboards[BoardType::Occupied as usize];
 
         Self { bitboards }
@@ -66,17 +70,103 @@ impl Board {
     }
 
     pub(crate) fn display_bitboard(&self, board_type: BoardType) {
-        let bit_board = self.bitboards[board_type as usize];
+        let bitboard = self.bitboards[board_type as usize];
 
         for rank in (0..8).rev() {
             for file in 0..8 {
                 let square = crate::utils::get_square_index(rank, file as u8);
-                let bit = (bit_board >> square) & 1;
+                let bit = (bitboard >> square) & 1;
 
                 print!("{}", if bit == 1 { "1" } else { "." })
             }
             println!();
         }
     }
-}
 
+    pub(crate) fn display_chess_board(&self) {
+        for rank in (0..8).rev() {
+            for file in 0..8 {
+                for (index, bitboard) in self.bitboards.iter().enumerate() {
+                    let square = crate::utils::get_square_index(rank, file as u8);
+                    let bit = (bitboard >> square) & 1;
+
+                    let board_type = BoardType::try_from(index);
+
+                    match board_type {
+                        Ok(BoardType::WhitePawns) => {
+                            if bit == 1 {
+                                print!("WP")
+                            }
+                        }
+                        Ok(BoardType::WhiteKnights) => {
+                            if bit == 1 {
+                                print!("WN")
+                            }
+                        }
+                        Ok(BoardType::WhiteBishops) => {
+                            if bit == 1 {
+                                print!("WB")
+                            }
+                        }
+                        Ok(BoardType::WhiteRooks) => {
+                            if bit == 1 {
+                                print!("WR")
+                            }
+                        }
+                        Ok(BoardType::WhiteQueens) => {
+                            if bit == 1 {
+                                print!("WQ")
+                            }
+                        }
+                        Ok(BoardType::WhiteKing) => {
+                            if bit == 1 {
+                                print!("WK")
+                            }
+                        }
+
+                        Ok(BoardType::BlackPawns) => {
+                            if bit == 1 {
+                                print!("BP")
+                            }
+                        }
+                        Ok(BoardType::BlackKnights) => {
+                            if bit == 1 {
+                                print!("BN")
+                            }
+                        }
+                        Ok(BoardType::BlackBishops) => {
+                            if bit == 1 {
+                                print!("BB")
+                            }
+                        }
+                        Ok(BoardType::BlackRooks) => {
+                            if bit == 1 {
+                                print!("BR")
+                            }
+                        }
+                        Ok(BoardType::BlackQueens) => {
+                            if bit == 1 {
+                                print!("BQ")
+                            }
+                        }
+                        Ok(BoardType::BlackKing) => {
+                            if bit == 1 {
+                                print!("BK")
+                            }
+                        }
+                        Ok(BoardType::Empty) => {
+                            if bit == 1 {
+                                print!(" ..")
+                            }
+                        }
+                        Ok(BoardType::Occupied)
+                        | Ok(BoardType::WhitePieces)
+                        | Ok(BoardType::BlackPieces) => continue,
+                        Err(_) => print!("??"),
+                    }
+                }
+            }
+            println!();
+        }
+    }
+}
--- engine/src/main.rs
+++ engine/src/main.rs
@@ -6,5 +6,6 @@ use crate::board::Board;
 fn main() {
     let board = Board::new();
 
-    board.display_bitboard(board::BoardType::BlackBishops);
+    //board.display_bitboard(board::BoardType::WhiteBishops);
+    board.display_chess_board();
 }
