513edf1c9db0390aba3a99d1fd8b3d24c43245df
Lukas Bauza <lukasbauza.lb@proton.me>
2026-03-11 09:57:01 +0000

    Better naming for bitboards

--- engine/src/board.rs
+++ engine/src/board.rs
@@ -1,76 +1,72 @@
 #[rustfmt::skip]
 #[derive(Copy, Clone)]
-pub(crate)enum PieceType {
+pub(crate)enum BoardType {
     WhitePawns = 0, WhiteKnights = 1, WhiteBishops = 2, WhiteRooks = 3, WhiteQueens = 4, WhiteKing = 5,
     BlackPawns = 6, BlackKnights = 7, BlackBishops = 8, BlackRooks = 9, BlackQueens = 10, BlackKing = 11,
 
     // Adding these will save time in calculation, as you don't need to count each array individually.
     WhitePieces = 12,
     BlackPieces = 13,
+    Empty = 14,
+    Occupied = 15
 }
 
 pub(crate) struct Board {
-    pieces: [u64; 14],
-    empty: u64,
-    occupied: u64,
+    bitboards: [u64; 16],
 }
 
 impl Board {
     pub(crate) fn new() -> Self {
-        let mut pieces = [0u64; 14];
+        let mut bitboards = [0u64; 16];
 
-        Self::set_rank_pieces([PieceType::WhitePawns; 8], 1, &mut pieces);
+        Self::set_rank_pieces([BoardType::WhitePawns; 8], 1, &mut bitboards);
         Self::set_rank_pieces(
             [
-                PieceType::WhiteRooks,
-                PieceType::WhiteKnights,
-                PieceType::WhiteBishops,
-                PieceType::WhiteQueens,
-                PieceType::WhiteKing,
-                PieceType::WhiteBishops,
-                PieceType::WhiteKnights,
-                PieceType::WhiteRooks,
+                BoardType::WhiteRooks,
+                BoardType::WhiteKnights,
+                BoardType::WhiteBishops,
+                BoardType::WhiteQueens,
+                BoardType::WhiteKing,
+                BoardType::WhiteBishops,
+                BoardType::WhiteKnights,
+                BoardType::WhiteRooks,
             ],
             0,
-            &mut pieces,
+            &mut bitboards,
         );
 
-        Self::set_rank_pieces([PieceType::BlackPawns; 8], 6, &mut pieces);
+        Self::set_rank_pieces([BoardType::BlackPawns; 8], 6, &mut bitboards);
         Self::set_rank_pieces(
             [
-                PieceType::BlackRooks,
-                PieceType::BlackKnights,
-                PieceType::BlackBishops,
-                PieceType::BlackQueens,
-                PieceType::BlackKing,
-                PieceType::BlackBishops,
-                PieceType::BlackKnights,
-                PieceType::BlackRooks,
+                BoardType::BlackRooks,
+                BoardType::BlackKnights,
+                BoardType::BlackBishops,
+                BoardType::BlackQueens,
+                BoardType::BlackKing,
+                BoardType::BlackBishops,
+                BoardType::BlackKnights,
+                BoardType::BlackRooks,
             ],
             7,
-            &mut pieces,
+            &mut bitboards,
         );
 
-        let occupied =
-            pieces[PieceType::WhitePieces as usize] | pieces[PieceType::BlackPieces as usize];
-        let empty = !occupied;
+        bitboards[BoardType::Occupied as usize] =
+            bitboards[BoardType::WhitePieces as usize] | bitboards[BoardType::BlackPieces as usize];
+        bitboards[BoardType::Empty as usize] = !bitboards[BoardType::Occupied as usize];
 
-        Self {
-            pieces,
-            occupied,
-            empty,
-        }
+        Self { bitboards }
     }
 
-    fn set_rank_pieces(piece_type: [PieceType; 8], rank: u8, pieces: &mut [u64; 14]) {
+    fn set_rank_pieces(piece_type: [BoardType; 8], rank: u8, bitboards: &mut [u64; 16]) {
         for (file, piece) in piece_type.iter().enumerate() {
             let sq = crate::utils::get_square_index(rank, file as u8);
-            pieces[*piece as usize] |= 1u64 << sq;
+            bitboards[*piece as usize] |= 1u64 << sq;
         }
     }
 
-    pub(crate) fn display_piece_bitboard(&self, piece_type: PieceType) {
-        let bit_board = self.pieces[piece_type as usize];
+    pub(crate) fn display_bitboard(&self, board_type: BoardType) {
+        let bit_board = self.bitboards[board_type as usize];
 
         for rank in (0..8).rev() {
             for file in 0..8 {
