e8e68d449d7a59cbed31df6d151f75c5e44e940a
Lukas Bauza <lukasbauza.lb@proton.me>
2026-03-16 16:37:31 +0000

    One square shifting
    One square shift

---
+++ src/movegen.rs
@@ -0,0 +1,36 @@
+use crate::types::Bitboard;
+
+const NOT_A_FILE: Bitboard = 0xfefefefefefefefe;
+const NOT_H_FILE: Bitboard = 0x7f7f7f7f7f7f7f7f;
+
+pub fn shift_north(bitboard: Bitboard) -> Bitboard {
+    bitboard << 8
+}
+
+pub fn shift_south(bitboard: Bitboard) -> Bitboard {
+    bitboard >> 8
+}
+
+pub fn shift_east(bitboard: Bitboard) -> Bitboard {
+    (bitboard << 1) & NOT_A_FILE
+}
+
+pub fn shift_west(bitboard: Bitboard) -> Bitboard {
+    (bitboard >> 1) & NOT_H_FILE
+}
+
+pub fn shift_north_east(bitboard: Bitboard) -> Bitboard {
+    (bitboard << 9) & NOT_A_FILE
+}
+
+pub fn shift_north_west(bitboard: Bitboard) -> Bitboard {
+    (bitboard << 7) & NOT_H_FILE
+}
+
+pub fn shift_south_east(bitboard: Bitboard) -> Bitboard {
+    (bitboard >> 7) & NOT_H_FILE
+}
+
+pub fn shift_south_west(bitboard: Bitboard) -> Bitboard {
+    (bitboard >> 9) & NOT_A_FILE
+}
