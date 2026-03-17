4602f6f2edeb83a454c9d961b4a7d9765e22a685
Lukas Bauza <lukasbauza.lb@proton.me>
2026-03-17 14:02:20 +0000

    Combine similar functions for pawn moves

--- src/pawnmove.rs
+++ src/pawnmove.rs
@@ -1,26 +1,26 @@
 use crate::movegen::*;
-use crate::types::Bitboard;
+use crate::types::{Bitboard, Color};
 
 const EMPTY: Bitboard = 0x0000000000000000;
 
-pub fn white_single_push_targets(pawns: Bitboard) -> Bitboard {
-    return shift_north(pawns) & EMPTY;
+pub fn get_single_push_targets(pawns: Bitboard, color: Color) -> Bitboard {
+    match color {
+        Color::White => return shift_north(pawns) & EMPTY,
+        Color::Black => return shift_south(pawns) & EMPTY,
+    }
 }
 
-pub fn white_double_push_targets(pawns: Bitboard) -> Bitboard {
-    const RANK4: Bitboard = 0x00000000FF000000;
-    let single_push = white_single_push_targets(pawns);
+pub fn get_double_push_targets(pawns: Bitboard, color: Color) -> Bitboard {
+    match color {
+        Color::White => {
+            const RANK4: Bitboard = 0x00000000FF000000;
 
-    return white_single_push_targets(single_push) & RANK4;
-}
+            return get_single_push_targets(pawns, color) & RANK4;
+        }
+        Color::Black => {
+            const RANK5: Bitboard = 0x000000FF00000000;
 
-pub fn black_single_push_targets(pawns: Bitboard) -> Bitboard {
-    return shift_south(pawns) & EMPTY;
-}
-
-pub fn black_double_push_targets(pawns: Bitboard) -> Bitboard {
-    const RANK5: Bitboard = 0x000000FF00000000;
-    let single_push = black_single_push_targets(pawns);
-
-    return black_single_push_targets(single_push) & RANK5;
+            return get_single_push_targets(pawns, color) & RANK5;
+        }
+    }
 }
