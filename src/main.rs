1f608c6353bd039fccd4141f85d5b3f84e6428c1
Lukas Bauza <lukasbauza.lb@proton.me>
2026-03-17 13:41:51 +0000

    Testing display bitboards and chessboard

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::board::Board;
+use crate::{board::Board, types::Color};
 
 mod board;
 mod movegen;
@@ -10,4 +10,6 @@ fn main() {
     let board: Board = Board::new();
 
     board.display_chess_board();
+
+    board.display_piece_bitboard(Color::Black, types::Piece::Pawn);
 }
