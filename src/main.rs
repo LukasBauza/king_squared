53423d314b2242559724a26183183b2515a81fbe
Lukas Bauza <lukasbauza.lb@proton.me>
2026-03-23 16:40:43 +0000

    Testing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
+mod board;
+mod move_gen;
+mod types;
+mod utils;
+
 use crate::{
     board::Board,
-    knightmove::get_knight_move_targets,
-    pawnmove::{get_attack_targets, get_double_push_targets, get_single_push_targets},
     types::{Color, Occupancy, Piece},
     utils::display_bitboard,
 };
-
-mod board;
-mod knightmove;
-mod movegen;
-mod pawnmove;
-mod types;
-mod utils;
 
 fn main() {
     let board: Board = Board::new();
 
     board.display_chess_board();
 
-    display_bitboard(get_knight_move_targets(
-        board.pieces[Color::Black as usize][Piece::Knight as usize],
-        board.occupancies[Occupancy::Empty as usize],
-    ));
+    display_bitboard(move_gen::pawn::get_double_push_targets(board, Color::White));
 }
